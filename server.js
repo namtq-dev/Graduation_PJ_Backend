ae2b9a47f40136e92b4588b27242bb29ed6edb43
namtq184161 <quangnam260800@gmail.com>
2022-11-28 22:13:32 +0700

    CORS config

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+// CORS config
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Welcome from home');

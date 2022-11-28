4bfc5ea164acd663008b4441215092297380d7d9
namtq184161 <quangnam260800@gmail.com>
2022-11-28 23:26:41 +0700

    set up mongodb

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const { readdirSync } = require('fs');
 
 dotenv.config();
+
+// database config
+connectDB().catch((err) => console.log(err));
+mongoose.connection.on(
+  'error',
+  console.error.bind(console, 'connection error:')
+);
+
+async function connectDB() {
+  await mongoose.connect(process.env.DATABASE_URL);
+  console.log('Connected to the MongoDB Server');
+}
 
 const app = express();
 
@@ -15,6 +28,7 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// routes config
 readdirSync('./routes').map((route) =>
   app.use('/', require('./routes/' + route))
 );
